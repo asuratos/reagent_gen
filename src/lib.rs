3eae28b9935e85966660c5e5419ed720341f5614
Aldrich Suratos <cyurio@gmail.com>
2022-06-14 11:29:56 +0900

    nonrandom builder working (todo: randomization of enum variants)

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,218 @@
+use once_cell::sync::Lazy;
+
+#[derive(Debug, PartialEq)]
 enum ReagentKind {
-    Plant
+    Plant,
 }
 
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
 enum ReagentProperty {
-    Restorative,
     Explosive,
-    Poisonous,
-    Combustible
+    Volatile,
+    Viscous,
 }
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum ReagentEffect {
+    Healing,
+    Strength,
+    Speed,
+    Clairvoyance,
+    StoneSkin,
+    Flight,
+    Invisibility,
+    Explosive,
+    Toxic,
+    Freezing,
+    Combusting,
+    Confusion,
+    Paralysis,
+    Blinding,
+    Flashing,
+    Viscous,
+    Volatile,
+    Hallucination,
+}
+
+static INCOMPATIBLES: Lazy<Vec<[ReagentEffect; 2]>> =
+    Lazy::new(|| vec![[ReagentEffect::Healing, ReagentEffect::Toxic]]);
+
+#[derive(Debug, PartialEq)]
 struct Reagent {
     kind: ReagentKind,
     name: String,
-    properties: Vec<ReagentProperty>
+    effects: Vec<ReagentEffect>,
+    property: Vec<ReagentProperty>,
+}
+
+#[derive(Debug, PartialEq)]
+struct ReagentBuilder {
+    kind: Option<ReagentKind>,
+    effects: Option<Vec<ReagentEffect>>,
+    property: Option<Vec<ReagentProperty>>,
+}
+
+impl ReagentBuilder {
+    pub fn new() -> ReagentBuilder {
+        ReagentBuilder {
+            kind: None,
+            effects: None,
+            property: None,
+        }
+    }
+
+    fn incomplete(&self) -> bool {
+        self.kind.is_none() || self.effects.is_none()
+    }
+
+    pub fn with_kind(mut self, kind: ReagentKind) -> ReagentBuilder {
+        self.kind = Some(kind);
+        self
+    }
+
+    fn add_property(&mut self, prop: ReagentProperty) {
+        if self.property.is_none() {
+            self.property = Some(vec![prop]);
+        } else {
+            if let Some(v) = self.property.as_mut() {
+                v.push(prop);
+                v.sort();
+                v.dedup();
+            };
+        }
+    }
+
+    pub fn with_property(mut self, prop: ReagentProperty) -> ReagentBuilder {
+        self.add_property(prop);
+        self
+    }
+
+    fn add_effect(&mut self, eff:ReagentEffect) {
+        if self.effects.is_none() {
+            self.effects = Some(vec![eff]);
+        } else {
+            if let Some(v) = self.effects.as_mut() {
+                v.push(eff);
+                v.sort();
+                v.dedup();
+            }
+        }
+    }
+
+    pub fn with_effect(mut self, eff: ReagentEffect) -> ReagentBuilder {
+        self.add_effect(eff);
+        self
+    }
+
+    pub fn build(self) -> Result<Reagent, String> {
+        //check if any field is None
+        if self.incomplete() {
+            return Err("Incomplete Build".to_string());
+        }
+
+        let reagent = Reagent {
+            name: "test".to_string(),
+            kind: self.kind.unwrap(),
+            effects: self.effects.unwrap(),
+            property: self.property.unwrap_or_else(Vec::new),
+        };
+
+        Ok(reagent)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn cant_build_from_new_builder() {
+        let inc = ReagentBuilder::new();
+
+        assert_eq!(inc.build(), Err("Incomplete Build".to_string()));
+    }
+
+    #[test]
+    fn cant_build_without_effects() {
+        let inc = ReagentBuilder::new().with_property(ReagentProperty::Explosive);
+
+        assert_eq!(inc.build(), Err("Incomplete Build".to_string()));
+    }
+
+    #[test]
+    fn can_build_without_properties() {
+        let inc = ReagentBuilder::new()
+            .with_effect(ReagentEffect::Healing)
+            .with_kind(ReagentKind::Plant);
+
+        assert!(inc.build().is_ok());
+    }
+
+    #[test]
+    fn adding_property() {
+        let builder = ReagentBuilder::new().with_property(ReagentProperty::Explosive);
+
+        assert_eq!(builder.property, Some(vec![ReagentProperty::Explosive]))
+    }
+    #[test]
+    fn adding_property_appends() {
+        let builder = ReagentBuilder::new()
+            .with_property(ReagentProperty::Explosive)
+            .with_property(ReagentProperty::Viscous);
+
+        assert_eq!(
+            builder.property,
+            Some(vec![ReagentProperty::Explosive, ReagentProperty::Viscous])
+        )
+    }
+
+    #[test]
+    fn adding_dup_property_doesnt_append() {
+        let builder = ReagentBuilder::new()
+            .with_property(ReagentProperty::Explosive)
+            .with_property(ReagentProperty::Explosive);
+
+        assert_eq!(
+            builder.property,
+            Some(vec![ReagentProperty::Explosive])
+        )
+    }
+
+    #[test]
+    fn adding_effect() {
+        let builder = ReagentBuilder::new().with_effect(ReagentEffect::Healing);
+
+        assert_eq!(builder.effects, Some(vec![ReagentEffect::Healing]))
+    }
+
+    #[test]
+    fn adding_effect_appends() {
+        let builder = ReagentBuilder::new()
+            .with_effect(ReagentEffect::Healing)
+            .with_effect(ReagentEffect::Speed);
+
+        assert_eq!(
+            builder.effects,
+            Some(vec![ReagentEffect::Healing, ReagentEffect::Speed])
+        )
+    }
+
+    #[test]
+    fn adding_dup_effect_doesnt_append() {
+        let builder = ReagentBuilder::new()
+            .with_effect(ReagentEffect::Healing)
+            .with_effect(ReagentEffect::Healing);
+
+        assert_eq!(
+            builder.effects,
+            Some(vec![ReagentEffect::Healing])
+        )
+    }
+
+    #[test]
+    fn complete_build() {
+        let builder = ReagentBuilder::new()
+            .with_effect(ReagentEffect::Healing)
+            .with_property(ReagentProperty::Explosive);
     }
 }
